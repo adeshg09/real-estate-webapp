8c70726c9679238aa778313bfa555009eeacd2c0
adesh09 <adeshgadage0908@gmail.com>
2025-10-01 04:08:24 +0530

    fix cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
 /* ROUTES */
 app.get("/", (req, res) => {
